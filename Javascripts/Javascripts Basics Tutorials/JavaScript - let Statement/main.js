ac3c9c400f0a7df871929edc7bc69070695d2e80
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-12 15:29:10 +0500

    up

---
+++ Javascripts/Javascripts Basics Tutorials/JavaScript - let Statement/main.js
@@ -0,0 +1,12 @@
+let name = "John";
+let age = 35;
+let x = true;
+
+let xy = 10;
+var y = 20;
+function test() {
+    let xy = 50;
+    var y = 100;
+    console.log("x = " + xy + ", y = " + y);
+}
+test();
