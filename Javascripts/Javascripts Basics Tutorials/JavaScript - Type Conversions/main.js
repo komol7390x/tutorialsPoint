11032921f4d21d7f897716a076f4325719c36ce7
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-12 18:09:17 +0500

    up

--- Javascripts/Javascripts Basics Tutorials/JavaScript - Type Conversions/main.js
+++ Javascripts/Javascripts Basics Tutorials/JavaScript - Type Conversions/main.js
@@ -1,6 +1,11 @@
+'use strict'
 let currentDate = new Date();         // Hozirgi sana va vaqt
 let dateNumber = Number(currentDate); // Sana vaqti raqamga aylantiriladi
 console.log(dateNumber);              // 1970-yil 1-yanvardan boshlab o'tgan millisekundalar soni
 
 let timeInMillis = currentDate.getTime(); // Sana vaqti millisekundlarda
 console.log(timeInMillis);       
+var a = 5;
+var a = 6;
+
+console.log(a);
