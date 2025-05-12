b67e918b166fed36f291c141045e743c36456d64
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-12 14:30:48 +0500

    up

---
+++ Javascripts/Javascripts Basics Tutorials/Javascripts-Console.log()/main.js
@@ -0,0 +1,12 @@
+console.log(1111);
+console.log("Hello World!");
+var num1 = 30;
+var num2 = 20;
+console.log("The sum of ", num1, " and ", num2, " is: ", num1 + num2);
+
+var obj = {
+    website: "Tutorialspoint",
+    Domain: "www.tutorialspoint.com",
+    description: "This site is for learning."
+};
+console.log(obj);
