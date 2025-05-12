e62d997e92488de7d16f17537b9a0cde06ae9cc7
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-12 14:38:09 +0500

    up

---
+++ Javascripts/Javascripts Basics Tutorials/Javascripts- Comments/main.js
@@ -0,0 +1,9 @@
+// Defining the string1
+var string1 = "JavaScript";
+// Defining the string2
+var string2 = "TypeScript";
+// Printing the strings
+console.log((string1 + " " + string2));   
+
+/* Comments xabarining birinchi qatori 
+Comments xabarining ikkinchi qatori */
