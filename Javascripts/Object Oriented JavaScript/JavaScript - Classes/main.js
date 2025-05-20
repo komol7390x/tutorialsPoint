fa046b513a5915f80be31921b477420f7b958ed6
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-20 10:36:13 +0500

    up

---
+++ Javascripts/Object Oriented JavaScript/JavaScript - Classes/main.js
@@ -0,0 +1,7 @@
+class Car {
+    constructor(brand) { // constructor metodini e’lon qilish
+        this.brand = brand; // xossaga qiymat berish
+    }
+}
+const myCar = new Car("BMW");
+console.log(myCar.brand); // Natija: BMW
