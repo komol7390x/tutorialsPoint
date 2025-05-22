7b0c21043436d0009aea82ca5c72fb0a98e7c43c
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-22 17:09:41 +0500

    up

--- Typescripts dars/main.ts
+++ Typescripts dars/main.ts
@@ -169,8 +169,86 @@ console.log(data4);
 */
 // -----------------------------------------------------------------------
 //7-dars  TUPPLE
+/*
 let arr3: [string, number] = ["k", 123];
 arr3.push(1223)
 console.log(arr3);
+*/
+// -----------------------------------------------------------------------
+//8-dars  ENUM
+/*
+enum En{
+    eng=3,
+    uzb=1,
+    rus=1
+}
+enum En1{
+    eng = "eng",
+    uzb = "uzbe",
+    rus=uzb
+}
+// console.log(En);
+// console.log(En1.eng);
+// ------------------------------------------
 
+enum Role {
+    admin = "Admin",
+    user = "User",
+    guest="Guest"
+  }
+  
+//   function showMessage(role: Role) {
+//       switch (role) {
+//           case Role.admin:
+//               return "Sizda barcha huquqlar bor."
+//           case Role.user:
+//               return "Siz oddiy foydalanuvchisiz."
+//           case Role.guest:
+//               return "Siz mehmon sifatida kirdingiz."
+//     }
+//   }
+function showMessage(role: Role): string{
+    const obj = {
+        [Role.admin]:"Sizda barcha huquqlar bor.",
+        [Role.guest]:"Siz mehmon sifatida kirdingiz.",
+        [Role.user]:"Siz oddiy foydalanuvchisiz."
+    }
+    return obj[role]
+}
+  console.log(showMessage(Role.user));
+  console.log(showMessage(Role.admin));
+  console.log(showMessage(Role.guest));
+*/
+// -----------------------------------------------------------------------
+//9-dars  ANY,UNKNOWN,NEVER
+// const getDate = (prop:(string | number | object)) => {
+//     console.log(prop);
+    
+// }
+const getDate = (prop: any) => {
+    console.log(prop);   //Istalgan qiymat qabul qilish uchun "any" oladi
+}
+// getDate("Komol"); 
+// getDate(1234);
+// getDate({name:"Komol"})
+  
+const getDate1 = (prop: unknown) => {  //malum turdagi type olish uchun unknown olinadi
+    if (typeof prop == "string") {
+        console.log( prop.length);
+    } else if (typeof prop == "number") {
+        console.log(prop);
+    } else if (typeof prop == "object") {
+        console.log(prop);
+    }
+}
+// getDate1("Komol");
+// getDate1(1234);
+// getDate1({name:"Komol"})
+  
+const getError = ():never => {
+    throw Error("Xatolik bor")
+}
+// console.log(getError()); //Xatolik Chiqadi
 
+let num: never;  //Hech qachon qiymat olmaydi
+// num=123,
