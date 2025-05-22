0d3b736e13dd12fe1f8d00db485a81644d9e6918
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-22 13:48:31 +0500

    up

--- Typescripts dars/main.ts
+++ Typescripts dars/main.ts
@@ -1,2 +1,67 @@
-let a = "Komol";
-console.log(a);
+//2-dars
+/*
+let a1: string = "Komol";
+console.log(a1);
+// -----------------------------------------
+let num: number = 12334332;
+console.log(num);
+// -----------------------------------------
+let big1:bigint=1234n
+console.log(big1);
+// -----------------------------------------
+let null1 = null
+console.log(null1);
+// -----------------------------------------
+let und1 = undefined
+console.log(und1);
+// -----------------------------------------
+let symb: symbol = Symbol("1223")
+console.log(symb);
+// -----------------------------------------
+let obj: { age: string } = { age: "typescrits" };
+console.log(obj);
+// -----------------------------------------
+let arr:number[] = [1,2,3,4,5,6,7,8];
+console.log(arr);
+// -----------------------------------------
+function getDate(name: string, age: number) {
+    return name
+}
+console.log(getDate("Komol",27).toUpperCase());
+// -----------------------------------------------------------------------
+*/
+//3-4-dars
+/*
+let num: number | string | boolean= true;  //Type larni berish ketish kerak
+// console.log( typeof num);
+// -----------------------------------------
+let num1: any = "123456"; //Ixtiyoriy qiymat bersa boladi
+// console.log(num1);
+// -----------------------------------------
+let lang: "eng" | "uzb" | "rus" = "rus";
+lang='eng'
+// console.log(lang);
+
+const data: {id?:number,value:"eng" | "uzb" | "rus"}[] = [
+    { id: 1, value:"uzb" },
+    {id:2,value:"eng"},
+    {id:3,value:"rus"}
+]
+console.log(typeof(lang));
+*/
+// -----------------------------------------------------------------------
+//5-dars
+type Lang = "eng" | "uzb" | "rus"
+type dataType = { id?: number, value: Lang }[]
+type Func = (title:string,age:number) => void;
+
+let lang: Lang = "rus";
+lang='eng'
+// console.log(lang);
+
+const data: dataType = [
+    { id: 1, value:"uzb" },
+    {id:2,value:"eng"},
+    {id:3,value:"rus"}
+]
+console.log(typeof(lang));
---
+++ Typescripts dars/src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let lang = "rus";
+lang = 'eng';
+// console.log(lang);
+const data = [
+    { id: 1, value: "uzb" },
+    { id: 2, value: "eng" },
+    { id: 3, value: "rus" }
+];
+console.log(typeof (lang));
