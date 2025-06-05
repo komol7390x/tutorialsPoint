77b88b675ae7c4d218b4c14dd45b091c06a2c36c
komol7390x <www.komol8689@gmail.com>
2025-06-05 14:15:53 +0500

    up

---
+++ Promise,await/event.js
@@ -0,0 +1,9 @@
+// const btn = document.getElementById("myBtn");
+
+// btn.addEventListener("click", function () {
+//     alert("Tugma bosildi!");
+// });
+let res = document.getElementById("mybtn");
+res.addEventListener("click", function () {
+    alert("Tugma bosilmadi!")
+})
