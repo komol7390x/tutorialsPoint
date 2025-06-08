84a3e3896b7c7565e08ba0768a2d366664110b0b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-08 19:01:35 +0500

    ip

--- Promise,await/event.js
+++ Promise,await/event.js
@@ -11,13 +11,13 @@ const newEmit = new EventEmitter();
 newEmit.on("Salom ber", (ism) => {
     console.log(`Salom, ${ism}!`);
 })
-newEmit.emit("Salom ber", "Komol")
+newEmit.emit("Salom ber", "Komol");
 
 // ------------------------------------------------
 const EventEmitter = require('node:events');
 class MyEmitter extends EventEmitter { }
 const myEmitter = new MyEmitter();
 myEmitter.on('event', function (a, b) {
-    console.log(a, b, this === myEmitter);
+    console.log(a, b, this === myEmitter)
 });
 myEmitter.emit('event', 'a', 'b');
