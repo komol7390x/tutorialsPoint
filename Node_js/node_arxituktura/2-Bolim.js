cee11bac3a13102b67ebb5d0b2980480404b66a6
komol7390x <www.komol8689@gmail.com>
2025-06-16 16:09:13 +0500

    up

--- Node_js/node_arxituktura/2-Bolim.js
+++ Node_js/node_arxituktura/2-Bolim.js
@@ -1,4 +1,21 @@
-// console.log(module) 
-const logger = require('./modul')
-logger.log('salom')
+
+const file = require('path');
+const pathObj = file.parse(__filename)
+
+// console.log(pathObj);
+// console.log(__filename);
+// --------------------------------------------
+// OS
+const os = require('os');
+const freeMem = os.totalmem();
+console.log('Xotirada bosh joy',Math.floor(freeMem/1024/1024),' mb');
+
+const userInfo = os.userInfo();
+console.log('Info',userInfo);
+
+
+// --------------------------------------------
+// console.log(module)
+// const logger = require('./modul')
+// logger.log('salom')
 // console.log(logger);
--- Node_js/node_arxituktura/modul.js
+++ Node_js/node_arxituktura/modul.js
@@ -1,5 +1,5 @@
-const url='https://192.168.7.1/logger'
-function loge(message) {
-    console.log(message);
-}
-module.exports = loge;
+// const url='https://192.168.7.1/logger'
+// function loge(message) {
+//     console.log(message);
+// }
+// module.exports = loge;
