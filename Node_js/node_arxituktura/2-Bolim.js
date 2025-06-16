729eceefc02eee343415b04b04e91ce1dafd8dfe
komol7390x <www.komol8689@gmail.com>
2025-06-16 16:16:08 +0500

    up

--- Node_js/node_arxituktura/2-Bolim.js
+++ Node_js/node_arxituktura/2-Bolim.js
@@ -8,10 +8,19 @@ const pathObj = file.parse(__filename)
 // OS
 const os = require('os');
 const freeMem = os.totalmem();
-console.log('Xotirada bosh joy',Math.floor(freeMem/1024/1024),' mb');
+// console.log('Xotirada bosh joy',Math.floor(freeMem/1024/1024),' mb');
 
-const userInfo = os.userInfo();
-console.log('Info',userInfo);
+// const userInfo = os.userInfo();
+// console.log('Info', userInfo);
+
+// console.log(os.arch()); //nechi bitligini biladi
+// console.log(os.platform()); //win32
+// console.log(os.cpus()[0]);  //cpu yadrosini
+// console.log(Math.floor(os.totalmem() / 1024 / 1024), ' mb'); //RAM Xotirasi jami;
+// console.log(Math.floor(os.freemem() / 1024 / 1024),' mb'); // RAM da bosh joy
+console.log(Math.floor(os.uptime()/60/60), 'soat');
+
+
 
 
 // --------------------------------------------
