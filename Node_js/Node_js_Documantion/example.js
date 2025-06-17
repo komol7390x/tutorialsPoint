2735c70d816a2db6d8d0459f687569c6fb24bdd0
komol7390x <www.komol8689@gmail.com>
2025-06-17 16:44:43 +0500

    up

--- Node_js/Node_js_Documantion/example.js
+++ Node_js/Node_js_Documantion/example.js
@@ -1,8 +1,9 @@
 const { spawn } = require('child_process');
-
-const calc = spawn('notepad.exe');
-
-calc.on('close', (code) => {
-    console.log(`Node ishga tushdi. Kod: ${code}`);
-});
+// const ls = spawn('ls', ['-lh', '/usr']); //Linux
+// // const dir = spawn('cmd.exe', ['/c', 'dir', 'C:\\Komol']); //berilgan papka haqida malumot
+// const dir = spawn('cmd.exe', ['/c', 'tasklist']);
+const dir = spawn('cmd.exe', ['/c', 'findstr', 'ssh']);
+dir.stdout.on('data', (data) => {
+    console.log(`${data}`);  
+})
 
--- Node_js/Node_js_Documantion/spawn()/spwan-2.js
+++ Node_js/Node_js_Documantion/spawn()/spwan-2.js
@@ -53,4 +53,14 @@ const calc = spawn('notepad.exe');
 calc.on('close', (code) => {
     console.log(`Node ishga tushdi. Kod: ${code}`);
 });
+// ------------------------------------------------------
+const { spawn } = require('child_process');
+// const ls = spawn('ls', ['-lh', '/usr']); //Linux
+// // const dir = spawn('cmd.exe', ['/c', 'dir', 'C:\\Komol']); //berilgan papka haqida malumot
+// const dir = spawn('cmd.exe', ['/c', 'tasklist']);
+const dir = spawn('cmd.exe', ['/c', 'findstr', 'ssh']);
+dir.stdout.on('data', (data) => {
+    console.log(`${data}`);
+})
+
 
