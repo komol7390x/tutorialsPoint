76a20de74a9f6efc97756e18e53a8c1c5e57b600
komol7390x <www.komol8689@gmail.com>
2025-06-17 17:02:32 +0500

    up

--- Node_js/Node_js_Documantion/example.js
+++ Node_js/Node_js_Documantion/example.js
@@ -1,9 +1,21 @@
-const { spawn } = require('child_process');
-// const ls = spawn('ls', ['-lh', '/usr']); //Linux
-// // const dir = spawn('cmd.exe', ['/c', 'dir', 'C:\\Komol']); //berilgan papka haqida malumot
-// const dir = spawn('cmd.exe', ['/c', 'tasklist']);
-const dir = spawn('cmd.exe', ['/c', 'findstr', 'ssh']);
-dir.stdout.on('data', (data) => {
-    console.log(`${data}`);  
-})
+const {spawn} = require('child_process');
 
+const prog = {
+    list: ['cmd', ['/c', 'dir']],
+    copy: ['cmd', ['/c', 'copy file1.txt file2.txt']],
+    folder: ['cmd', ['/c', 'mkdir new2_folder']]
+};
+
+const child = spawn(...prog.folder); // '...' bilan arrayni ajratamiz
+
+child.stdout.on('data', (data) => {
+    console.log(`Natija:\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`Xatolik:\n${data}`);
+});
+
+child.on('close', (code) => {
+    console.log(`Jarayon tugadi. Kod: ${code}`);
+});
