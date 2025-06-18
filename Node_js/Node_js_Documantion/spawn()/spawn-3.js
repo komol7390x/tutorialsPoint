7f5598b82bfa6699721512132031c84ace50158e
komol7390x <www.komol8689@gmail.com>
2025-06-18 16:14:37 +0500

    up

--- Node_js/Node_js_Documantion/spawn()/spawn-3.js
+++ Node_js/Node_js_Documantion/spawn()/spawn-3.js
@@ -1,32 +1,32 @@
-// const { spawn } = require('child_process');
+const { spawn } = require('child_process');
 
-// const pythonProcess = spawn('python', ['my_script.py']);
+const pythonProcess = spawn('python', ['my_script.py']);
 
-// // Python skriptiga ma'lumot yuboramiz
-// pythonProcess.stdin.write('hello from nodejs\n');
-// pythonProcess.stdin.write('another message\n');
-// pythonProcess.stdin.end(); // Ma'lumot yuborishni tugatamiz
+// Python skriptiga ma'lumot yuboramiz
+pythonProcess.stdin.write('hello from nodejs\n');
+pythonProcess.stdin.write('another message\n');
+pythonProcess.stdin.end(); // Ma'lumot yuborishni tugatamiz
 
-// // Python skriptidan javobni tinglaymiz
-// pythonProcess.stdout.on('data', (data) => {
-//     console.log(`Python stdout: ${data.toString()}`);
-// });
+// Python skriptidan javobni tinglaymiz
+pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python stdout: ${data.toString()}`);
+});
 
-// // Xatoliklarni tinglaymiz
-// pythonProcess.stderr.on('data', (data) => {
-//     console.error(`Python stderr: ${data.toString()}`);
-// });
+// Xatoliklarni tinglaymiz
+pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data.toString()}`);
+});
 
-// // Python skripti tugaganda
-// pythonProcess.on('close', (code) => {
-//     console.log(`Python process exited with code ${code}`);
-// });
+// Python skripti tugaganda
+pythonProcess.on('close', (code) => {
+    console.log(`Python process exited with code ${code}`);
+});
 
-// pythonProcess.on('error', (err) => {
-//     console.error('Failed to start Python process.', err);
-// });
+pythonProcess.on('error', (err) => {
+    console.error('Failed to start Python process.', err);
+});
 
-// console.log('Python script is running...');
+console.log('Python script is running...');
 
 // --------------------------------------------------------------
 
