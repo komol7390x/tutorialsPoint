3be1a75322462d17f9779c3d6e572684eb80f587
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-06 11:29:00 +0500

    up

---
+++ Node_js/node_arxituktura/2-15-dars.js
@@ -0,0 +1,10 @@
+// import helmet from 'helmet'
+// import morgan from 'morgan'
+import add_math_ar from 'add_math_ar/'
+// import path from 'path'
+
+// const Path = path.join()
+
+// const res = addMath
+// console.log(addMath(2, 3));
+
