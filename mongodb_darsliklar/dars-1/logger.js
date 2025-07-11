b099eb641f510ea5743e8bdc320843ea7eb68e9f
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-11 23:18:00 +0500

    up

--- mongodb_darsliklar/dars-1/logger.js
+++ mongodb_darsliklar/dars-1/logger.js
@@ -1,10 +1,21 @@
-const url = 'http://192.168.7.1/logger'
-
-function log(message) {
+export const url = 'http://192.168.7.1/logger'
+import EventEmitter from 'events'
+// --------------------------------------------------------------------------------------
+// 13-dars
+export const log = (message) => {
     // to-do : bu yerda messagani url ga http post qilin jonatish kodi bolish kerak
     console.log(message);
 }
 
-export {
-    log, url
+// --------------------------------------------------------------------------------------
+// 14-dars
+export class Logger extends EventEmitter {
+    log1(message) {
+        this.emit('emit1', message)
+    }
+    log2(message) {
+        this.emit('emit2', message)
+    }
 }
+// --------------------------------------------------------------------------------------
+// 15-dars
