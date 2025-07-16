6720462d325cbd36fcf41c3735abb9f7bd3ceea3
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-16 14:56:13 +0500

    up

---
+++ NodeJS/4-Topshiriq/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import {config} from 'dotenv'
+import {join} from 'path'
+
+import categoriesRoute from './routes/categories.js'
+
+const pathEnv=join(process.cwd(),'../../.env')
+config({path:pathEnv})
+const server = express();
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('Server is connect to database');
+
+    } catch (error) {
+        console.log(`error to connect database`, error.message);
+        process.exit(1)
+    }
+}
+
+await connectDB()
+server.use(express.json());
+server.use('/api/categories', categoriesRoute);
+
+const port = +process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(port,`chi portni eshitishni boshladim...`);
+});
