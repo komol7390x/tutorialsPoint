e27a4ff76e6b9cc452cbf2a28c989d96fb879fe9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-18 17:03:56 +0500

    up

---
+++ NodeJS/12-Topshiriq/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import categoriesRoute from './routes/categories.js'
+import customersRoute from './routes/customers.js'
+import coursesRoute from './routes/courses.js'
+import entrollmentsRoute from './routes/enrollments.js'
+import usersRoute from './routes/users.js'
+import { connectDB } from './configs/database.js';
+
+await connectDB()
+const app = express();
+
+app.use(express.json());
+app.use('/api/categories', categoriesRoute);
+app.use('/api/customers', customersRoute);
+app.use('/api/courses', coursesRoute);
+app.use('/api/enrollments', entrollmentsRoute);
+app.use('/api/users', usersRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${port}chi portni eshitishni boshladim...`);
+});
