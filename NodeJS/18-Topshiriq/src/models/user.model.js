07ece363ba9c5c637604bd765413c25770ad85eb
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-22 16:49:41 +0500

    up

---
+++ NodeJS/18-Topshiriq/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  }
+});
+
+export const User = mongoose.model('user', userSchema);
