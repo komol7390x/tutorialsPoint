e1c5461092fd72b6bf825b74c151d2b8bf4310b9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-24 16:47:33 +0500

    up

--- NodeJS/23-Topshiriq/src/tests/unit/models/user.test.js
+++ NodeJS/23-Topshiriq/src/tests/unit/models/user.test.js
@@ -1,12 +1,12 @@
-const {User} =require('../../../models/user.model')
-const tokenValid = require('../../../utils/Token')
-const jwt = require('jsonwebtoken');
-const {configFile} =require('../../../config/env.config')
-
+import { User } from '../../../models/user.model';
+import tokenValid from '../../../models/user.model.js'
+import jwt from 'jsonwebtoken';
+import { configFile } from '../../../config/env.config';
+ 
 describe('user.generateAuthToken', () => {
     it('should return a valid JWT', () => {
         const user = new User({ isAdmin: true });
-        const token = user.generateAuthToken();
+        const token = tokenValid.AccessToken(user,configFile.TOKEN.ACCESS_TOKEN_KEY)
         const decodedObject = jwt.verify(token, configFile.TOKEN.ACCESS_TOKEN_KEY);
         expect(decodedObject).toMatchObject({ isAdmin: true });
     });
