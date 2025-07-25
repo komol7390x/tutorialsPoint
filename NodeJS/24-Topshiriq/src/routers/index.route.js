466108b9115a84fe4f2bf115ab353e5635c8036f
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-25 15:15:08 +0500

    up

--- NodeJS/24-Topshiriq/src/routers/index.route.js
+++ NodeJS/24-Topshiriq/src/routers/index.route.js
@@ -1,23 +1,23 @@
-import 'express-async-errors';
+// import 'express-async-errors';
 import { Router } from 'express'
-import { winstonFunc } from '../utils/Winston.js';
-import categoriesRoute from './categories.route.js'
-import customersRoute from './customers.route.js'
-import coursesRoute from './courses.route.js'
-import enrollmentsRoute from './enrollments.route.js'
-import usersRoute from './users.route.js'
-import { winstonError } from '../middleware/error.js';
+// import { winstonFunc } from '../utils/Winston.js';
+// import categoriesRoute from './categories.route.js'
+// import customersRoute from './customers.route.js'
+// import coursesRoute from './courses.route.js'
+// import enrollmentsRoute from './enrollments.route.js'
+// import usersRoute from './users.route.js'
+// // import { winstonError } from '../middleware/error.js';
 
-winstonFunc()
+// // winstonFunc()
 const router = Router();
 
-router.use('/categories', categoriesRoute);
-router.use('/customers', customersRoute);
-router.use('/courses', coursesRoute);
-router.use('/enrollments', enrollmentsRoute);
-router.use('/users', usersRoute);
+// router.use('/categories', categoriesRoute);
+// router.use('/customers', customersRoute);
+// router.use('/courses', coursesRoute);
+// router.use('/enrollments', enrollmentsRoute);
+// router.use('/users', usersRoute);
 
-router.use(winstonError)
+// // router.use(winstonError)
 
 export default router;
 
--- NodeJS/24-Topshiriq/src/start/production.js
+++ NodeJS/24-Topshiriq/src/start/production.js
@@ -1,7 +1,9 @@
 import helmet from "helmet";
 import comprasion from 'compression'
 
-export default  production=async (app)=>{
+const  production=async (app)=>{
     app.use(helmet());
     app.use(comprasion());
 }
+
+export default production
