52e02de3e06db9b895233d32e2ab2d3a7878f263
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-31 15:12:46 +0500

    up

--- src/error/global-error-handle.js
+++ src/error/global-error-handle.js
@@ -1,7 +1,7 @@
 export const globalErrorHandle = (err, _req, res, _next) => {
     console.log('Error global file: ', err.message);
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal server error';
+    const message = err.message || 'Internal server error';    
     return res.status(statusCode).json({
         statusCode,
         message
